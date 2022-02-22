27bdee013629d281b40065fefc8a9ac31c649149
Floris Weijler <Floris.weijler@ing.com>
2022-02-22 16:36:41 +0100

    Exercise 4

--- jobs/app.py
+++ jobs/app.py
@@ -22,6 +22,10 @@ def execute_sql(sql, values=(), commit = False, single = False):
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
 
+    
+    cursor.close()
+    return results
+
 @app.teardown_appcontext
 def close_connection(exeption):
     connection = getattr(g, '_connection', None)
